0b809bda969f9ec21f21b00e15bc2616f3034f18
Muhammad Lutfi Salinggih <lutfisalinggih@gmail.com>
2025-05-29 16:47:56 +0700

    feature: bring model to integrate
    tf model masih blm di push karena perlu  configure git lfs

--- model/app.py
+++ model/app.py
@@ -1,8 +1,21 @@
+import tensorflow as tf
+import re
+import string
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from transformers import TFAutoModelForSequenceClassification, AutoTokenizer
+from indoNLP.preprocessing import replace_word_elongation, replace_slang
 
 app = Flask(__name__)
 CORS(app)  # biar bisa diakses dari backend Hapi di port lain
+
+# Load model & tokenizer
+model_path = "./saved_model1"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = TFAutoModelForSequenceClassification.from_pretrained(model_path)
+
+labels = ['anger', 'fear', 'happy', 'joy', 'love', 'sadness', 'surprise']
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
@@ -14,22 +27,47 @@ def analyze():
     return jsonify(emotions)
 
 def analyze_story(story):
-    # ini data dummy nanti tim ml ubah disini yaaa
-    story_lower = story.lower()
-    emotions = {
-        "happy": story_lower.count("happy") * 10 + 10,
-        "sad": story_lower.count("sad") * 10,
-        "angry": story_lower.count("angry") * 10,
-        "neutral": 100,  # asumsi netral 100 dulu, nanti bisa dikurangi oleh emosi lain
-    }
+    #membersihkan teks
+    text = cleaningText(story)
+    
+    # Tokenisasi input
+    inputs = tokenizer(text, return_tensors="tf", truncation=True, padding=True)
 
-    # Hitung total emosi 
-    total_non_neutral = sum(emotions[k] for k in ["happy", "sad", "angry"])
+    # Prediksi dari model
+    outputs = model(inputs)
+    logits = outputs.logits.numpy()[0]
 
-    # total 100%
-    emotions["neutral"] = max(0, 100 - total_non_neutral)
+    # Softmax jadi persentase
+    probabilities = tf.nn.softmax(logits).numpy()
+    percentages = (probabilities * 100).round(2)
+
+    # Buat dictionary: label -> persen
+    emotions = {label: float(percentages[i]) for i, label in enumerate(labels)}
 
     return emotions
+
+def cleaningText(text):
+    # Membersihkan tanda tanda sisa medsos
+    text = re.sub(r'@[A-Za-z0-9]+', '', text)  # Menghapus Mention
+    text = re.sub(r'#[A-Za-z0-9]+', '', text)  # Menghapus Hashtag
+    text = re.sub(r'RT[\s]', '', text)  # menghapus RT
+    text = re.sub(r"http\S+", '', text)  # menghapus link
+
+    # Pembersihan Karakter
+    text = text.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation))) # Mengganti tanda baca dengan spasi (alih-alih menghapus)
+    text = re.sub(r'\d+', '', text) # Menghapus angka
+    text = text.replace('\n', ' ') # Mengganti garis baru dengan spasi
+
+    # Normalisasi
+    text = text.strip(' ') # Menghapus karakter spasi dari kiri dan kanan text
+    text = text.lower()  # mengubah semua karakter dalam text menjadi huruf kecil
+    text = re.sub(r'(.)\1+', r'\1\1', text) # Menghapus huruf berlebih di belakang
+    text = replace_word_elongation(text) # Menghapus text elongation (library indoNLP)
+    text = replace_slang(text) # Menghapus slangwords dari kamus IndoNLP
+    #text = remove_stopwords(text)  # menghapus kata yang tidak penting
+    text = re.sub(r'\s+', ' ', text) # Mengganti multiple spasi dengan satu spasi
+    return text
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
